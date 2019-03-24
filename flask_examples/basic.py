50c367bbd5d46a3a5e14ae0c8e2ae870b5d937dd
Felipe <feload@gmail.com>
2019-03-24 11:47:22 -0600

    Enables debug mode

--- flask_examples/basic.py
+++ flask_examples/basic.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return '<h1>Root route</h1>'
 
+@app.route('/info')
+def info():
+    return '<h1>Information.</h1>'
+
+@app.route('/profile/<name>')
+def profile(name):
+    return 'User: {0}'.format(name[100])
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)   # DEBUG Should be disabled in production environments.

6c207dc74f8350a4ae36df61709284e53fcb582d
Felipe <feload@gmail.com>
2019-03-24 19:24:26 -0600

    Add simple form and 404 error handler.

--- flask_examples/basic.py
+++ flask_examples/basic.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,11 +9,20 @@ def index():
 
 @app.route('/info')
 def info():
-    return '<h1>Information.</h1>'
+    return render_template('info.html')
+
+@app.route('/thank-you')
+def thank_you():
+    email = request.args.get('email')
+    return render_template('thank-you.html', email = email)
 
 @app.route('/profile/<name>')
 def profile(name):
     return 'User: {0}'.format(name)
+
+@app.errorhandler(404)
+def fourofour(e):
+    return render_template('404.html')
 
 if __name__ == '__main__':
     app.run(debug = True)   # DEBUG Should be disabled in production environments.
